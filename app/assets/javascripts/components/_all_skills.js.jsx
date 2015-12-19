4cacfd5634fa00f8ba604876f4aa58a632d96b7b
Jeffrey Ruane <ruane.jb@gmail.com>
2015-12-19 12:18:49 -0700

    Render new skill on page without reload

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,12 +1,6 @@
 var AllSkills = React.createClass({
-  getInitialState() {
-    return { skills: [] }
-  },
-  componentDidMount() {
-    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
-  },
   render() {
-    var skills = this.state.skills.map((skill, index) => {
+    var skills = this.props.skills.map((skill, index) => {
       return (
         <div key={index}>
           <h3>{skill.name}</h3>
